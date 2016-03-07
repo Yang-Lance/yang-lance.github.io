aa1600c3ee5cd5f7e2d21a64572510ac749a8ffc
Yangholmes Liang <Yangholmes Liang>
2016-03-07 17:38:26 +0800

    add some fns in information.js~

--- information/information.js
+++ information/information.js
@@ -25,24 +25,69 @@ function getServerTime(myReq) {
   myRand = parseInt(Math.random()*999999999999999);
   var theURL = thePage +"?rand="+myRand;
   myReq.open("GET", theURL, true);
-  myReq.onreadystatechange = theHTTPResponse(myReq);
+  myReq.onreadystatechange = function() {
+	  if (myReq.readyState == 4) {
+			console.log(myReq);
+		if(myReq.status == 200) {
+			console.log(myReq);
+		   var timeString = myReq.responseXML.getElementsByTagName("timestring")[0];
+		   document.getElementById('showtime').innerHTML = timeString.childNodes[0].nodeValue;
+		}
+	  } else {
+		console.log(myReq);
+		document.getElementById('showtime').innerHTML = '<p>wait...</p>';
+	  }
+	};
   myReq.send(null);
 }
 
-function theHTTPResponse(myReq) {
-  if (myReq.readyState == 4) {
-		console.log(myReq);
-    if(myReq.status == 200) {
-		console.log(myReq);
-       var timeString = myReq.responseXML.getElementsByTagName("timestring")[0];
-       document.getElementById('showtime').innerHTML = timeString.childNodes[0].nodeValue;
-    }
-  } else {
-	console.log(myReq);
-    document.getElementById('showtime').innerHTML = '<p>wait...</p>';
-  }
-}
+
 function onMouseClick(){
 	var myReq = getXMLHTTPRequest();
 	getServerTime(myReq);
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * 标题列表事件注册
+ */
+function listRegister(callback){
+	var listDiv = document.getElementById("information-list");
+	var informationList = listDiv.getElementsByTagName("li");
+	for( var i=0;i<informationList.length;i++ ){
+		console.log(informationList[i]);
+		informationList[i].onclick = function(){
+			//this: informationList[i]
+			callback(this);
+		}
+	}
+}
+
+/**
+ * 生成标题列表
+ * @param titleLists: Array, list of titles
+ */
+function listGenerate(titleLists){
+	var listDiv = document.getElementById("information-list");
+	var titleListHTML = "";
+	for( var i=0;i<titleLists.length;i++){
+		titleListHTML = titleListHTML + "<li>" + titleLists[i] + "</li>" ;
+	}
+	console.log(titleListHTML);
+	listDiv.innerHTML = titleListHTML;
+}
+
+/**
+ * Use JSON decode to send a HTML POST request
+ * @param callback: function that used to deal with request
+ */
+function postJSON(url, data, callback){
+	var request = new XMLHttpRequest();
+	request.open("POST", url);
+	request.onreadystatechange = function(){
+		callback(request);
+	};
+	request.setRequestHeader("Content-Type", "application/json");
+	request.sent( JSON.stringify(data) );
 }
